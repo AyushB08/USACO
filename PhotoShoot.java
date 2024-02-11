7b7c419be560633b7f74ca92bac1b2a1d8e48a45
AyushB08 <102773944+AyushB08@users.noreply.github.com>
2024-02-11 11:06:37 -0800

    Update PhotoShoot.java

--- PhotoShoot.java
+++ PhotoShoot.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//PROBLEM LINK: http://www.usaco.org/index.php?page=viewproblem2&cpid=988
 import java.util.*;
 import java.io.*;
 public class Main {
